4564b4f7a43803ceef5646b5ed95276acf2b4d73
Gabriel-F36 <gabriel.emil.johansson@gmail.com>
2023-09-20 14:38:57 +0200

    Add project files.

---
+++ Checkpoint-2 Nivå1/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+List<Product> productList = new List<Product>();
+string cat;
+string prod;
+string pri;
+int value;
+
+while (true)
+{
+    Console.Write("Category: ");
+    cat = Console.ReadLine();
+    if (cat.ToLower().Trim() == "q")
+    {
+        break;
+    }
+    
+    Console.Write("Product Name: ");
+    prod = Console.ReadLine();
+    if (prod.ToLower().Trim() == "q")
+    {
+        break;
+    }
+
+    tryPriceAgain:
+    Console.Write("Price: ");
+    pri = Console.ReadLine();
+    if (pri.ToLower().Trim() == "q")
+    {
+        break;
+    }
+    if (!int.TryParse(pri, out value))
+    {
+        Console.WriteLine("It wasn't a valid number, try again");
+        goto tryPriceAgain;
+    }
+        
+    productList.Add(new Product(cat, prod, value));
+    Console.WriteLine("Product added");
+}
+
+// Printing the productlist in specific format
+
+Console.WriteLine("Category".PadRight(10) + "ProductName".PadRight(14) + "Price");
+foreach (Product p in productList)
+{
+    Console.WriteLine(p.Category.PadRight(10) + p.ProductName.PadRight(14) + p.Price);
+}
+
+
+Console.ReadLine();
+
+
+class Product
+{
+    public Product()
+    {
+    }
+
+    public Product(string category, string productName, int price)
+    {
+        Category = category;
+        ProductName = productName;
+        Price = price;
+    }
+
+    public string Category { get; set; }
+    public string ProductName { get; set; }
+    public int Price { get; set; }
+}
+
+
+
+
+
+
+
