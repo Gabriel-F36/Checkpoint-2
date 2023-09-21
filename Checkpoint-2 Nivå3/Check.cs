d19fef1c7adb08e051a86c24251c083ee42806ef
Gabriel-F36 <gabriel.emil.johansson@gmail.com>
2023-09-21 16:51:12 +0200

    Version 1.3 nivå 3

---
+++ Checkpoint-2 Nivå3/Check.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint_2_Nivå3
+{
+    internal class Check
+    {
+        public bool Quit(string St)
+        {
+            if (St.ToLower().Trim() == "q")
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public bool Null(string St)
+        {
+            if (St.ToLower().Trim() == "" || St.ToLower().Trim() == null)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public bool NewProduct(string St)
+        {
+            if (St.ToLower().Trim() == "p")
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public bool Search(string St)
+        {
+            if (St.ToLower().Trim() == "s")
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+    }
+}
--- Checkpoint-2 Nivå3/Program.cs
+++ Checkpoint-2 Nivå3/Program.cs
@@ -2,11 +2,12 @@
 
 using Checkpoint_2_Nivå1;
 using Checkpoint_2_Nivå2;
+using Checkpoint_2_Nivå3;
 using System.Diagnostics;
 using System.Linq.Expressions;
 
 AppMessages appMessages = new AppMessages();
-appMessages.Instructions();
+Check check = new Check();
 List<Product> productList = new List<Product>();
 
 string category;
@@ -14,42 +15,113 @@ string productName;
 string price;
 int value;
 
+//static bool QuitCheck(string St)
+//{
+//    if (St.ToLower().Trim() == "q")
+//    {
+//        return true;
+//    }
+//    else { return false; }
+//}
+//static bool NullCheck(string St)
+//{
+//    if (St.ToLower().Trim() == "" || St.ToLower().Trim() == null)
+//    {
+//        return true;
+//    }
+//    else { return false; }
+//}
+//static bool NewProduct(string St)
+//{
+//    if (St.ToLower().Trim() == "p")
+//    {
+//        return true;
+//    }
+//    else { return false; }
+//}
+
+addExtraProduct:
 while (true)
 {
+    appMessages.Instructions();
+
+tryCategoryAgain:
     Console.Write("Category: ");
     category = Console.ReadLine();
-    if (category.ToLower().Trim() == "q")
+    if (check.Null(category) == true)
+    {
+        appMessages.NullOrEmpty();
+        goto tryCategoryAgain;
+    }
+    if (check.Quit(category) == true)
     {
         break;
     }
-
+    
+  tryProductAgain:
     Console.Write("Product Name: ");
     productName = Console.ReadLine();
-    if (productName.ToLower().Trim() == "q")
+    if (check.Null(productName) == true)
+    {
+        appMessages.NullOrEmpty();
+        goto tryProductAgain;
+    }
+    if (check.Quit(productName) == true)
     {
         break;
     }
 
-tryPriceAgain:
+  tryPriceAgain:
     Console.Write("Price: ");
     price = Console.ReadLine();
-    if (price.ToLower().Trim() == "q")
+    if (check.Null(price) == true)
+    {
+        appMessages.NullOrEmpty();
+        goto tryPriceAgain;
+    }
+    if (check.Quit(price) == true)
     {
         break;
     }
     if (!int.TryParse(price, out value))
     {
-        Console.WriteLine("It wasn't a valid number, try again");
+        appMessages.TryAgain();
         goto tryPriceAgain;
     }
 
     productList.Add(new Product(category, productName, value));
-    Console.WriteLine("Product added");
+    appMessages.successfulAdd();
 }
 
 List<Product> sortedByPrice = productList.OrderBy(x => x.Price).ToList();
 
 new Printing().PrintTableOfContent(sortedByPrice);
+
+appMessages.LateAddProduct();
+
+while (true)
+{
+  tryNewAgain:
+    string data = Console.ReadLine();
+    if (check.NewProduct(data) == true) 
+    {
+        goto addExtraProduct;
+    }
+    if (check.Null(data) == true)
+    {
+        appMessages.NullOrEmpty();
+        goto tryNewAgain;
+    }
+    if (check.Quit(data) == true)
+    {
+        break;
+    }
+    if (check.Search(data) == true)
+    {
+        // TODO figure out how to search
+        Console.WriteLine("TODO figure out how to search");
+    }
+}
 
 
 Console.ReadLine();
